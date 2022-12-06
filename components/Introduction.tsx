63ee84e0905a1b18a9abfcda48c36bd09d4c59c6
Valentin1495 <newface95@icloud.com>
2022-12-06 23:08:48 +0900

    2nd commit

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -1,6 +1,8 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
 
 export default function Introduction() {
+  const { data: session } = useSession();
+
   return (
     <div className=" bg-[#EDF2F7] px-4 py-9">
       <div className="flex flex-col gap-y-3 max-w-2xl mx-auto">
@@ -21,50 +23,58 @@ export default function Introduction() {
           comment below. After the comment is approved, it will display below.
         </p>
         <div className="mt-10 mb-5 sm:space-x-2 flex flex-col gap-y-2 sm:block">
-          <button className="btn" onClick={() => signIn("google")}>
-            <svg
-              viewBox="0 0 533.5 544.3"
-              focusable="false"
-              role="presentation"
-              aria-hidden="true"
-              className="w-5 h-5 icon"
-            >
-              <g>
-                <path
-                  d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
-                  fill="#4285f4"
-                ></path>
-                <path
-                  d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"
-                  fill="#34a853"
-                ></path>
-                <path
-                  d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"
-                  fill="#fbbc04"
-                ></path>
-                <path
-                  d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
-                  fill="#ea4335"
-                ></path>
-              </g>
-            </svg>
-            Continue with Google
-          </button>
-          <button className="btn" onClick={() => signIn("twitter")}>
-            <svg
-              viewBox="0 0 24 24"
-              aria-hidden="true"
-              className="w-6 h-6 icon"
-            >
-              <g>
-                <path
-                  fill="rgb(29, 155, 240)"
-                  d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"
-                ></path>
-              </g>
-            </svg>
-            Continue with Twitter
-          </button>
+          {!session ? (
+            <>
+              <button className="btn" onClick={() => signIn("google")}>
+                <svg
+                  viewBox="0 0 533.5 544.3"
+                  focusable="false"
+                  role="presentation"
+                  aria-hidden="true"
+                  className="w-5 h-5 icon"
+                >
+                  <g>
+                    <path
+                      d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
+                      fill="#4285f4"
+                    ></path>
+                    <path
+                      d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"
+                      fill="#34a853"
+                    ></path>
+                    <path
+                      d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"
+                      fill="#fbbc04"
+                    ></path>
+                    <path
+                      d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
+                      fill="#ea4335"
+                    ></path>
+                  </g>
+                </svg>
+                Continue with Google
+              </button>
+              <button className="btn" onClick={() => signIn("twitter")}>
+                <svg
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                  className="w-6 h-6 icon"
+                >
+                  <g>
+                    <path
+                      fill="rgb(29, 155, 240)"
+                      d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"
+                    ></path>
+                  </g>
+                </svg>
+                Continue with Twitter
+              </button>
+            </>
+          ) : (
+            <button className="bg-black text-white text-lg font-bold px-3 py-1.5 rounded-md hover:text-black hover:bg-white duration-700 w-fit mx-auto sm:mx-0">
+              View Dashboard
+            </button>
+          )}
         </div>
       </div>
     </div>
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -14,6 +14,4 @@ export default NextAuth({
       version: "2.0",
     }),
   ],
-
-  secret: process.env.SECRET,
 });
